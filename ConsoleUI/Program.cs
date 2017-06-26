7a85585e299cd4c689b6d788d699b2932f91a620
Kiril Shchasny <kiril3467@yandex.ru>
2017-06-26 17:45:26 +0300

    Add quick sort logic and consoleUI

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using QucikSortLogic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[15] { 8, 17, 8, 4, 82, 59, 97, 61, 35, 10, 34, 48, 27, 75, 60 };
+            Output(arr);
+            Console.WriteLine();
+            QuickSortLogic.QuickSort(arr);
+            Output(arr);
+            Console.ReadKey();
+        }
+
+        static void Output(int[] arr)
+        {
+            foreach (int x in arr)
+                Console.Write(x + " ");
+        }
+    }
+}
